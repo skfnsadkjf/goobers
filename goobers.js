3c04bac2e7d4b28e38abe82f63c5e57fd86a7090
amfl <amfl@none.none>
2020-01-17 21:44:04 +1300

    Add entity

--- goobers.js
+++ goobers.js
@@ -1,19 +1,29 @@
 console.log('hello')
 
+let hero = {'pos': [0,0]}
+
 let map_dimensions = 10
 let grid = Array.from({length: map_dimensions}, () => {
     return Array.from({length: map_dimensions}, () =>
-        Math.random() > 0.2 ? ' ' : '#'
+        Math.random() > 0.2 ? '.' : '#'
     )
 })
 
-function print_map(map) {
-    map.forEach(row => {
-        console.log(row.toString())
+function print_map(map, entities) {
+    map.forEach((row, y) => {
+        let s = row
+        entities.forEach(e => {
+            if (e.pos[1] == y) {
+                s[e.pos[0]] = '%'
+            }
+        })
+        console.log(s.join(''))
         // row.forEach(v => {
         //     console.log(v)
         // })
     })
 }
 
-print_map(grid)
+
+
+print_map(grid, [hero])
