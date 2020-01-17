ecab1ab3f93294f1c4e7d5c0df5cbd8df115c5ef
amfl <amfl@none.none>
2020-01-17 19:15:53 +1300

    Create simple map, print it out

--- goobers.js
+++ goobers.js
@@ -1,1 +1,19 @@
 console.log('hello')
+
+let map_dimensions = 10
+let grid = Array.from({length: map_dimensions}, () => {
+    return Array.from({length: map_dimensions}, () =>
+        Math.random() > 0.2 ? ' ' : '#'
+    )
+})
+
+function print_map(map) {
+    map.forEach(row => {
+        console.log(row.toString())
+        // row.forEach(v => {
+        //     console.log(v)
+        // })
+    })
+}
+
+print_map(grid)
