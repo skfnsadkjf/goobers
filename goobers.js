4f3d7a58f43afd30e7a2199d49c8cfce1af455b2
amfl <amfl@none.none>
2020-01-17 22:43:35 +1300

    Add esdf movement and collision detection

--- goobers.js
+++ goobers.js
@@ -3,7 +3,7 @@ console.log('hello')
 let hero = {'pos': [0,0]}
 
 let map_dimensions = 10
-let grid = Array.from({length: map_dimensions}, () => {
+let map = Array.from({length: map_dimensions}, () => {
     return Array.from({length: map_dimensions}, () =>
         Math.random() > 0.2 ? '.' : '#'
     )
@@ -22,24 +22,52 @@ function print_map(map, entities) {
     })
 }
 
+function tileIsPassable( map , coords ) {
+    if (coords[0] >= 0 &&
+        coords[1] >= 0 &&
+        coords[0] < map_dimensions &&
+        coords[1] < map_dimensions) {
+        return map[coords[1]][coords[0]] != "#";
+    } else {
+        // Out of bounds
+        return false
+    }
+}
+function add(c1, c2) {
+    return [c1[0] + c2[0], c1[1] + c2[1]]
+}
+function attemptMove(map, hero, delta) {
+    let new_coords = add(hero.pos, delta)
+    if (tileIsPassable(map, new_coords)) {
+        hero.pos = new_coords;
+    }
+}
+
 document.addEventListener('keydown', (e) => {
     console.log('keydown')
     console.log(e)
-    if ( e.key == "f" && hero.pos[0] < map_dimensions - 1 ) {
-        hero.pos[0]++;
+    if ( e.key == "f") {
+        attemptMove(map, hero, [1, 0])
     }
-    console.log( hero.pos );
-    if ( e.key == "s" && hero.pos[0] > 0 ) {
-        hero.pos[0]--;
+    if ( e.key == "s") {
+        attemptMove(map, hero, [-1, 0])
     }
-    if ( e.key == "d" && hero.pos[1] < map_dimensions - 1 ) {
-        hero.pos[1]++;
+    if ( e.key == "d") {
+        attemptMove(map, hero, [0, 1])
     }
-    if ( e.key == "e" && hero.pos[1] > 0 ) {
-        hero.pos[1]--;
+    if ( e.key == "e") {
+        attemptMove(map, hero, [0, -1])
     }
-    print_map( grid , [hero] );
+    // if ( e.key == "s" && hero.pos[0] > 0 ) {
+    //     hero.pos[0]--;
+    // }
+    // if ( e.key == "d" && hero.pos[1] < map_dimensions - 1 ) {
+    //     hero.pos[1]++;
+    // }
+    // if ( e.key == "e" && hero.pos[1] > 0 ) {
+    //     hero.pos[1]--;
+    // }
+    print_map( map , [hero] );
 })
 
-
-print_map(grid, [hero])
+print_map(map, [hero])
