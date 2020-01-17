672b2b9e663ad4cb70cb8b0eea299eb168472a94
amfl <amfl@none.none>
2020-01-17 23:31:37 +1300

    Make map a class

--- goobers.js
+++ goobers.js
@@ -2,51 +2,54 @@ console.log('hello')
 
 let hero = {'pos': [0,0]}
 
-let map = {
-    "dimensions": 10,
-    "data" : generateMap( 10 )
-}
-function generateMap( dimensions ) {
-    return Array.from({length: dimensions}, () => {
-        return Array.from({length: dimensions}, () =>
-            Math.random() > 0.2 ? '.' : '#'
-        )
-    })
-}
+class Map {
+    constructor(dimensions) {
+        this.dimensions = dimensions
+        this.data = Array.from({length: dimensions}, () => {
+            return Array.from({length: dimensions}, () =>
+                Math.random() > 0.2 ? '.' : '#'
+            )
+        })
+    }
+    get(coords) {
+        return this.data[coords[0]][coords[1]];
+    }
+    print(entities) {
+        // Build up an array of strings which will represent the map
+        let print_arr = Array.from( { "length" : this.dimensions } , v => "" );
+        this.data.forEach( ( col , x ) => {
+            col.forEach( ( v , y ) => {
+                let drawhero = entities.some(v => {
+                    return v.pos[0] == x && v.pos[1] == y
+                })
+                print_arr[y] += drawhero ? '%' : v
 
-function printMap(map, entities) {
-    // Build up an array of strings which will represent the map
-    let print_arr = Array.from( { "length" : map.dimensions } , v => "" );
-    map.data.forEach( ( col , x ) => {
-        col.forEach( ( v , y ) => {
-            drawhero = entities.some(v => {
-                return v.pos[0] == x && v.pos[1] == y
-            })
-            print_arr[y] += drawhero ? '%' : v
-
+            } );
         } );
-    } );
-    // Print our array of strings
-    console.log( print_arr.join( "\n" ) );
-}
-
-function tileIsPassable( map , coords ) {
-    if (coords[0] >= 0 &&
-        coords[1] >= 0 &&
-        coords[0] < map.dimensions &&
-        coords[1] < map.dimensions) {
-        return map.data[coords[0]][coords[1]] != "#";
-    } else {
-        // Out of bounds
-        return false
+        // Print our array of strings
+        console.log( print_arr.join( "\n" ) );
+    }
+    tileIsPassable( coords ) {
+        if (coords[0] >= 0 &&
+            coords[1] >= 0 &&
+            coords[0] < this.dimensions &&
+            coords[1] < this.dimensions) {
+            return this.get(coords) != "#";
+        } else {
+            // Out of bounds
+            return false
+        }
     }
 }
+
+let map = new Map(10)
+
 function add(c1, c2) {
     return [c1[0] + c2[0], c1[1] + c2[1]]
 }
 function attemptMove(map, hero, delta) {
     let new_coords = add(hero.pos, delta)
-    if (tileIsPassable(map, new_coords)) {
+    if (map.tileIsPassable(new_coords)) {
         hero.pos = new_coords;
     }
 }
@@ -75,7 +78,7 @@ document.addEventListener('keydown', (e) => {
     // if ( e.key == "e" && hero.pos[1] > 0 ) {
     //     hero.pos[1]--;
     // }
-    printMap( map , [hero] );
+    map.print( [hero] );
 })
 
-printMap(map, [hero])
+map.print( [hero] );
